541146d3ad8d2e6af4da17e77be15c3ad5b961e5
Zach <jzluci00@gmail.com>
2023-11-03 18:39:59 -0500

    reading and writing from text files now and added printList() method

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-todos =[]
-
-
 while True:
-    userChoice = input("Type add, show, or exit: ")
+    userChoice = input("Type add, show, edit, complete, or exit: ")
     userChoice = userChoice.strip()
+
+    def printList():
+        file = open('todos.txt', 'r')
+        todos = file.readlines()
+        file.close()
+        for i, item in enumerate(todos, start=1):
+            print(f"{i}. {item}")
 
     match userChoice:
         case 'add':
-            todo = input("Enter Todo: ")
+            todo = input("Enter Todo: ") + "\n"
+
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+
             todos.append(todo)
+
+            file = open('todos.txt', 'w')
+            file.writelines(todos)
+            file.close()
         case "show":
-            for item in todos:
-                print(item)
+            printList()
+        case "edit":
+            printList()
+            choice = int(input("Select number of To Do to edit: ")) - 1
+            update = input("Enter new To Do: ")
+            todos[choice] = update
+        case "complete":
+            printList()
+            choice = int(input("Select number of To Do to complete: ")) - 1
+            todos.pop(choice)
         case 'exit':
             break
+
+print("Bye!")
