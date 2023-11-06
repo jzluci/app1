16b4b5561230bae83d03519d9462177f17f8695a
jzluci <jzluci00@gmail.com>
2023-11-06 16:59:50 -0600

    Added edit feature. 14:50 in day 17 162

--- gui.py
+++ gui.py
@@ -4,9 +4,14 @@ import PySimpleGUI as psg
 label = psg.Text("Type in a to-do: ")
 input_box = psg.InputText(tooltip="Enter to-do", key="todo")
 add_button = psg.Button("Add")
+list_box = psg.Listbox(values=functions.get_todos(),
+                       key="items",
+                       enable_events=True,
+                       size = [45, 10])
+edit_button = psg.Button("Edit")
 
 window = psg.Window("My To-Do App",
-                    layout=[[label], [input_box, add_button]],
+                    layout=[[label], [input_box, add_button], [list_box, edit_button]],
                     font=('Helvetica', 20))
 while True:
     event, values = window.read()
@@ -18,6 +23,15 @@ while True:
             todos = functions.get_todos()
             todos.append(values['todo'] + '\n')
             functions.set_todos(todos)
+        case "Edit":
+            todo_to_edit = values['items'][0]
+            new_todo = values['todo']
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.set_todos(todos)
+            window['items'].update(values=todos)
         case psg.WIN_CLOSED:
             break
 
