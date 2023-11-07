2077539fdcdc0f910c24cfd8c48d0e2b43e77f4c
Zach <jzluci00@gmail.com>
2023-11-07 16:41:01 -0600

    added date time and error handling pop ups

--- gui.py
+++ gui.py
@@ -1,9 +1,14 @@
 import functions
 import PySimpleGUI as psg
+import time
 
+psg.theme("DarkPurple4")
+
+
+clock = psg.Text('', key='clock')
 label = psg.Text("Type in a to-do: ")
 input_box = psg.InputText(tooltip="Enter to-do", key="todo")
-add_button = psg.Button("Add")
+add_button = psg.Button("Add", size=10)
 list_box = psg.Listbox(values=functions.get_todos(),
                        key="items",
                        enable_events=True,
@@ -13,13 +18,15 @@ complete_button = psg.Button("Complete")
 exit_button = psg.Button("Exit")
 
 window = psg.Window("My To-Do App",
-                    layout=[[label],
+                    layout=[[clock],
+                            [label],
                             [input_box, add_button],
                             [list_box, edit_button, complete_button],
                             [exit_button]],
                     font=('Helvetica', 20))
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout=500)
+    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     print(event)
     print(values)
 
@@ -30,14 +37,17 @@ while True:
             functions.set_todos(todos)
             window['items'].update(values=todos)
         case "Edit":
-            todo_to_edit = values['items'][0]
-            new_todo = values['todo']
+            try:
+                todo_to_edit = values['items'][0]
+                new_todo = values['todo']
 
-            todos = functions.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            functions.set_todos(todos)
-            window['items'].update(values=todos)
+                todos = functions.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                functions.set_todos(todos)
+                window['items'].update(values=todos)
+            except IndexError:
+                psg.popup("Please select a value first.", font=("Helvetica", 20))
         case "items":
             window['todo'].update(value=values['items'][0])
         case "Complete":
