037ee866a1abeffeabbec4f6030a356f718fd98d
Zach <jzluci00@gmail.com>
2023-11-07 20:14:50 -0600

    finished implementing completing todos. It is complete for the time being

--- web.py
+++ web.py
@@ -13,14 +13,15 @@ todos = functions.get_todos()
 st.title("My Todo App")
 st.subheader("This is my to-do app")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.set_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="",
               placeholder="Add new todo",
               key='new_todo',
               on_change=add_todo)
-
-print("Hello")
-
-st.session_state
