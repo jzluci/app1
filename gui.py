a9e0a361389475a06c1d911f15670a3f62ac05f9
Zach <jzluci00@gmail.com>
2023-11-07 19:21:36 -0600

    added date time and error handling pop ups

--- gui.py
+++ gui.py
@@ -1,6 +1,11 @@
 import functions
 import PySimpleGUI as psg
 import time
+import os
+
+if not os.path.exists("todos.txt"):
+    with open("todos.txt", 'w') as file:
+        pass
 
 psg.theme("DarkPurple4")
 
